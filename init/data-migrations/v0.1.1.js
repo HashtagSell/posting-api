4770c2a615ffb6f9847fe3d17570bee923762c58
Joshua Thomas <joshua.thomas@gmail.com>
2015-02-12 06:10:34 -0800

    creating migration file to adjust index in running db

---
+++ init/data-migrations/v0.1.1.js
@@ -0,0 +1,22 @@
+var
+cursor,
+	version = 'v0.1.1';
+
+// ensure versions table and index exists
+db.versions.ensureIndex({ version : 1 }, { unique : true });
+
+// create query to determine if this version exists
+cursor = db.versions.find({ version : version }).limit(1);
+
+// check if version exists
+if (!cursor.hasNext()) {
+	// remove unique constraint on 3taps id
+	db.postings.dropIndex({ 'external.threeTaps.id' : 1 });
+	db.postings.ensureIndex({ 'external.threeTaps.id' : 1 });
+
+	// insert version for future migration runs
+	db.versions.insert({
+		appliedAt : new Date(),
+		version : version
+	});
+}
