b37137f30414345e370a710872a8f24a8b32658f
Joshua Thomas <joshua.thomas@gmail.com>
2015-02-18 09:17:39 -0800

    fixing migration script

--- init/data-migrations/v0.1.4.js
+++ init/data-migrations/v0.1.4.js
@@ -17,8 +17,29 @@ if (!cursor.hasNext()) {
 	// remove 2d index on geo.coordinates
 	db.postings.dropIndex({ 'geo.coordinates' : '2d' });
 
-	// re-add constraint correctly
-	db.postings.ensureIndex({ 'geo.coordinates' : true }, { '2dsphere' : true });
+	// reverse lat/long to be long/lat for all postings
+	db.postings.find().forEach(function (posting) {
+		var coords = posting.geo ? posting.geo.coordinates : undefined;
+
+		// set coords if empty
+		if (typeof coords === 'undefined') {
+			coords = [0, 0];
+		}
+
+		// reverse long and lat so that long is first element
+		coords.splice(0, 2, coords[1], coords[0]);
+
+		db.postings.update({
+			postingId : posting.postingId
+		}, {
+			$set : {
+				'geo.coordinates': coords
+			}
+		});
+	});
+
+	// re-add index as 2dsphere
+	db.postings.ensureIndex({ 'geo.coordinates' : '2dsphere' });
 
 	// insert version for future migration runs
 	db.versions.insert({
